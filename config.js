5f252bfb10e9dc92b19b9e3f5bb27e788fcce880
zgf <chriszh123@sina.com>
2018-12-03 22:02:00 +0800

    serverhost

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 配置文件
 const config = {
-    // serverHost: "https://zlz.herokuapp.com"
-    serverHost: "http://localhost:8888"
+    serverHost: "https://zlz.herokuapp.com"
+    // serverHost: "http://localhost:8888"
 };
 
 export default config;
